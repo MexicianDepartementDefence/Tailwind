04d64e30de21216c21b6acb3d635d71e053711f8
MexicianDepartementDefence <faisal216093@gmail.com>
2023-03-11 14:21:55 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./HTML/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        warnabiawak : "red"
+      },
+backgroundImage: {
+  'Bambang': 'url("../img/image (1).png")'
+},
+    },
+  },
+  plugins: [],
+}
